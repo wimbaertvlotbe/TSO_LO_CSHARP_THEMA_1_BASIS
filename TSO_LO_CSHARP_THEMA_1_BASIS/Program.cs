2573a983ad8a74ce9bd204b09d4257cd0110d4d4
wimbaertvlotbe <36698691+wimbaertvlotbe@users.noreply.github.com>
2021-09-19 11:19:09 +0200

    Add project files.

---
+++ TSO_LO_CSHARP_THEMA_1_BASIS/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace TSO_LO_CSHARP_THEMA_1_BASIS
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+    /* *****************************************************************************
+       1. GEGEVEN: In een programma moet een gebruiker een getal van het type Double 
+          ingeven. Foutieve invoer wordt niet aanvaard. 
+          GEVRAAGD: Schrijf de code die de beschreven gebruikersinvoer afhandelt. 
+
+            •Je kiest een geschikte naam voor de functie(s)
+            •Je kiest correcte argumenten voor de functie(s)
+            •Je retourneert een zinvolle resultaten naar de aanroepende code
+      
+       ***************************************************************************** */
+
+
+
+    //                      schrijf hier de code voor opdracht 1
+
+
+
+
+    /* *****************************************************************************
+       2. GEGEVEN: In een programma wordt er door een gebruiker een onder- en een 
+          bovengrens ingegeven. De ingegeven waarden zijn van het type int. 
+          De ondergrens moet uiteraard kleiner zijn dan de bovengrens. 
+          GEVRAAGD: Schrijf de code die nagaat of de ingevoerde ondergrens 
+          kleiner is dan de bovengrens.
+
+            •Je kiest een geschikte naam voor de functie(s)
+            •Je kiest correcte argumenten voor de functie(s)
+            •Je retourneert een zinvolle resultaten naar de aanroepende code
+
+       ***************************************************************************** */
+
+
+
+    //                      schrijf hier de code voor opdracht 2
+
+
+
+    /* *****************************************************************************
+       3.GEGEVEN: Twee tweedimensionale n x m array’s worden gevuld met random 
+         integer getallen tussen 0 en 255. De twee array’s worden elkaar vergeleken. 
+         Er wordt geteld hoeveel posities in beide array’s hetzelfde getal bevatten. 
+
+          GEVRAAGD: 
+
+            Schrijf de code waarmee je de array’s kan vullen met random getallen. 
+            Schrijf de code waarmee je de array’s met elkaar kan vergelijken.
+
+            •Je kiest een geschikte naam voor de functie(s)
+            •Je kiest correcte argumenten voor de functie(s)
+            •Je retourneert een zinvolle resultaten naar de aanroepende code
+
+       ***************************************************************************** */
+
+
+
+    //                      schrijf hier de code voor opdracht 3
+
+
+
+}
